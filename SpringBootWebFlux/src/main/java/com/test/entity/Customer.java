c75a92f21ddfe4a317d8de6d8c4235702085df8a
shubhamkumar260904 <shubhamkumar260904@gmail.com>
2026-04-06 16:56:29 +0530

    Add files via upload

---
+++ SpringBootWebFlux/src/main/java/com/test/entity/Customer.java
@@ -0,0 +1,43 @@
+package com.test.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+//@Entity
+@Table(name = "customers1")
+public class Customer {
+
+	@Id
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	
+	private String city;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+}
