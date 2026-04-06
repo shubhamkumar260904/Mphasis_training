5e1ba29b148a3ff8d2dd42dd4ba3434522cd56ea
shubhamkumar260904 <shubhamkumar260904@gmail.com>
2026-04-06 15:57:36 +0530

    Add files via upload

---
+++ ContextnConfig/src/main/java/com/test/Servlet1.java
@@ -0,0 +1,40 @@
+package com.test;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+//@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("Text/html");
+		PrintWriter out=response.getWriter();
+		/*String name=request.getParameter("fname");
+		String company=request.getParameter("cmp");
+		String lc=request.getParameter("loc");*/
+		
+		ServletContext ctx=getServletContext();
+		String ct=ctx.getInitParameter("city");
+		String st=ctx.getInitParameter("state");
+		String data=ct+" "+st;
+		ctx.setAttribute("info", data);
+		ServletConfig cfg=getServletConfig();
+		String usr=cfg.getInitParameter("user");
+		String psd=cfg.getInitParameter("pas");
+		out.println(usr+" "+psd+" From Servlet-1");
+		out.println("from servlet-1 :"+data);
+		out.println("<a href='Servlet2'> next page </a>"); 
+	}
+
+}
